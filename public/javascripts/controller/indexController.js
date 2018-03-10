fce143c7770c1304dce675edb4a9eca62ffa8e68
Mehmet Seven <mehmetseven0@gmail.com>
2018-03-10 21:07:22 +0300

    newUser

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,15 +1,27 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
 
-	const connectionOptions = {
-		reconnectionAttempts: 3,
-		reconnectionDelay: 600
+	$scope.init = () => {
+		const username = prompt('Please enter username');
+
+		if (username)
+			initSocket(username);
+		else
+			return false;
 	};
 
-	indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-		.then((socket) => {
-			console.log('bağlantı gerçekleşti', socket);
-		}).catch((err) => {
-			console.log(err);
-		});
+
+	function initSocket(username) {
+		const connectionOptions = {
+			reconnectionAttempts: 3,
+			reconnectionDelay: 600
+		};
+
+		indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+			.then((socket) => {
+				socket.emit('newUser', { username });
+			}).catch((err) => {
+				console.log(err);
+			});
+	}
 
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -1,11 +1,26 @@
 const socketio = require('socket.io');
 const io = socketio();
-const socketApi = { };
 
+const socketApi = { };
 socketApi.io = io;
+
+const users = [ ];
 
 io.on('connection', (socket) => {
 	console.log('a user connected');
+
+	socket.on('newUser', (data) => {
+		const defaultData = {
+			id: socket.id,
+			position: {
+				x: 0,
+				y: 0
+			}
+		};
+
+		const userData = Object.assign(data, defaultData);
+		users.push(userData);
+	});
 });
 
 module.exports = socketApi;
