eeba4d1ca9013279ad751b906a5652a714e92904
Mehmet Seven <mehmetseven0@gmail.com>
2018-03-13 16:18:38 +0300

    chat scroll

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -91,6 +91,11 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
 
 					$scope.messages.push(messageData);
 					$scope.message = '';
+
+					setTimeout(() => {
+						const element = document.getElementById('chat-area');
+						element.scrollTop = element.scrollHeight;
+					});
 				};
 
 			}).catch((err) => {
