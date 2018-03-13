865146f78d7c03b4bc9abb1497c0b72daf2c2298
Mehmet Seven <mehmetseven0@gmail.com>
2018-03-13 18:33:22 +0300

    bug fix

--- src/socketApi.js
+++ src/socketApi.js
@@ -34,14 +34,19 @@ io.on('connection', (socket) => {
 	});
 
 	socket.on('animate', (data) => {
-		users[socket.id].position.x = data.x;
-		users[socket.id].position.y = data.y;
+		try{
+			users[socket.id].position.x = data.x;
+			users[socket.id].position.y = data.y;
 
-		socket.broadcast.emit('animate', {
-			socketId: socket.id,
-			x: data.x,
-			y: data.y
-		});
+			socket.broadcast.emit('animate', {
+				socketId: socket.id,
+				x: data.x,
+				y: data.y
+			});
+		}catch(e){
+			console.log(e);
+		}
+
 	});
 
 	socket.on('newMessage', data => {
